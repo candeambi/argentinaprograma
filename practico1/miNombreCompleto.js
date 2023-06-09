b014f0e25ca97b18fb23b9b3ad5f5b84f1f1f9b5
candeambi <candeambi@gmail.com>
2023-06-08 22:15:12 -0300

    Agrego archivo miNombreCompleto.js

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -1,1 +1,17 @@
+// a. Defina una variable nombre.
+// b. Guarde en la variable nombre tu nombre real.
 
+let nombre = "Candela";
+
+// c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
+
+let apellido = "Ambi";
+
+// d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
+
+let edad = 25;
+
+// e. Por último, imprime por consola un mensaje que respete el siguiente formato:
+// "Hola, [nombre] [apellido]. Tienes [edad] años."
+
+console.log ("Hola, %s %s. Tienes %i años.", nombre, apellido, edad);
