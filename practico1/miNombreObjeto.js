5850036f18078d472189505b871acb405ad0f2c3
candeambi <candeambi@gmail.com>
2023-06-09 00:05:04 -0300

    Agregue archivo miNombreObjeto.js

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,11 @@
+// a. Defina una variable de tipo objeto que lleve el nombre persona, y que
+// contenga las variables nombre, apellido y edad del ejercicio anterior con
+// sus respectivos valores.
+
+let persona = {nombre: "Candela", apellido: "Ambi", edad: 25};
+
+// b. Imprime por consola un mensaje que respete el siguiente formato:
+//“Mi objeto persona es el siguiente: [persona]”
+
+console.log("Mi objeto persona es el siguiente: " + JSON.stringify(persona)); 
+
