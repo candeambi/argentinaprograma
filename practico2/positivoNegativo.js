a9f349e01dc49a44a5fe1ae43692c89c5ca93a2f
candeambi <candeambi@gmail.com>
2023-06-20 20:39:43 -0300

    Agrego archivo positivoNegativo.js

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,20 @@
+// Ejercicio 1. b. Solicite al usuario ingresar un número. Si el número es positivo, muestra
+// por consola el mensaje: "El numero es positivo". Si el número es igual a
+// cero, muestra por consola el mensaje: "El numero es cero". Si el número
+// es negativo, muestra por consola el mensaje: "El numero es negativo".
+// Guardar el programa en un archivo con nombre positivoNegativo.js
+
+const readlineSync = require ("readline-sync");
+let numero = readlineSync.question ("Ingrese un numero: ");
+
+let positivoNegativo;
+    if (numero > 0){
+        console.log("El numero es positivo");
+    } else if (numero == 0) {
+        console.log ("El numero es cero");
+    } else if (numero < 0) {
+        console.log("El numero es negativo");
+    };
+
+
+console.log(positivoNegativo);
