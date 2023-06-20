8e13cb0165d4992f2f6201de67140bc60a817578
candeambi <candeambi@gmail.com>
2023-06-20 20:38:32 -0300

    Agrego archivo mayorDeEdad.js

--- practico2/mayorDeEdad.js
+++ practico2/mayorDeEdad.js
@@ -1,1 +1,17 @@
+// Ejercicio 1. a. Solicite al usuario ingresar su edad. Si la edad es mayor o igual a 18,
+// muestra por consola el mensaje: "Eres mayor de edad". De lo contrario,
+// muestra por consola el mensaje: "Eres menor de edad". Guardar en un
+// archivo llamado mayorDeEdad.js
+
+const readlineSync = require ("readline-sync");
+let edad = readlineSync.question ("¿Cuál es tu edad?: ");
+
+let mayorDeEdad;
+    if (edad >= 18){
+        console.log("Eres mayor de edad");
+    } else {
+        console.log("Eres menor de edad");
+    };
+
+console.log(mayorDeEdad);
 
